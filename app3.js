6f71c4b0505e96ac90bdc9ed7fc97e757ecc9af9
Alexander Royal <alexander.de.royal@gmail.com>
2023-05-19 15:33:15 -0500

    working on coffee project

---
+++ app3.js
@@ -0,0 +1,15 @@
+// document.getElementById('#search-input').addEventListener('input', filterList);
+// function filterList(){
+//     const searchInput = document.querySelector('#search-input');
+//     const filter = searchInput.value.toLowerCase();
+//     const listItems = document.querySelectorAll('.list-group-item');
+//
+//     listItems.forEach((item) => {
+//         let text = item.textContent;
+//         if(text.toLowerCase().includes(filter.toLowerCase())){
+//             item.style.display = '';
+//         } else {
+//             item.style.display = 'none';
+//         }
+//     });
+// }
--- main.js
+++ main.js
@@ -2,40 +2,50 @@
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 let coffees = [
-    {id: 1, name: 'Light City', roast: 'Light', roasted: 'All'},
-    {id: 2, name: 'Half City', roast: 'Light', roasted: 'All'},
-    {id: 3, name: 'Cinnamon', roast: 'Light', roasted: 'All'},
-    {id: 4, name: 'City', roast: 'Medium', roasted: 'All'},
-    {id: 5, name: 'American', roast: 'Medium', roasted: 'All'},
-    {id: 6, name: 'Breakfast', roast: 'Medium', roasted: 'All'},
-    {id: 7, name: 'High', roast: 'Dark', roasted: 'All'},
-    {id: 8, name: 'Continental', roast: 'Dark', roasted: 'All'},
-    {id: 9, name: 'New Orleans', roast: 'Dark', roasted: 'All'},
-    {id: 10, name: 'European', roast: 'Dark', roasted: 'All'},
-    {id: 11, name: 'Espresso', roast: 'Dark', roasted: 'All'},
-    {id: 12, name: 'Viennese', roast: 'Dark', roasted: 'All'},
-    {id: 13, name: 'Italian', roast: 'Dark', roasted: 'All'},
-    {id: 14, name: 'French', roast: 'Dark', roasted: 'All'}
+    {id: 1, name: 'Light City', roast: 'Light', roasted: 'All', price: '$2.50',},
+    {id: 2, name: 'Half City', roast: 'Light', roasted: 'All', price: '$2.50'},
+    {id: 3, name: 'Cinnamon', roast: 'Light', roasted: 'All', price: '$2.50'},
+    {id: 4, name: 'City', roast: 'Medium', roasted: 'All', price: '$2.50'},
+    {id: 5, name: 'American', roast: 'Medium', roasted: 'All', price: '$2.50'},
+    {id: 6, name: 'Breakfast', roast: 'Medium', roasted: 'All', price: '$2.50'},
+    {id: 7, name: 'High', roast: 'Dark', roasted: 'All', price: '$2.50'},
+    {id: 8, name: 'Continental', roast: 'Dark', roasted: 'All', price: '$2.50'},
+    {id: 9, name: 'New Orleans', roast: 'Dark', roasted: 'All', price: '$2.50'},
+    {id: 10, name: 'European', roast: 'Dark', roasted: 'All', price: '$2.50'},
+    {id: 11, name: 'Espresso', roast: 'Dark', roasted: 'All', price: '$2.50'},
+    {id: 12, name: 'Viennese', roast: 'Dark', roasted: 'All', price: '$2.50'},
+    {id: 13, name: 'Italian', roast: 'Dark', roasted: 'All', price: '$2.50'},
+    {id: 14, name: 'French', roast: 'Dark', roasted: 'All', price: '$2.50'}
 ];
-
-//THIS FUNCTION CREATES TABLE
-// function renderCoffee(coffee) {
-//     let html = '<tr class="coffee">';
-//     html += '<td>' + coffee.id + '</td>';
-//     html += '<td>' + coffee.name + '</td>';
-//     html += '<td>' + coffee.roast + '</td>';
-//     html += '</tr>';
-//     return html;
-// }
 
 //Tables are a little old school, you need to refactor the code so that each coffee is displayed in a div that contains a heading displaying the coffee name, and the type of roast in a paragraph. Don't display the ids, these are only for our application's internal use
 function renderCoffee(coffee){
-    let html = '<div class="coffee">';
-        // html += '<p>' + coffee.id + '</p>';
+
+    if(coffee.roast === 'Light'){
+        let html = '<div class="coffee col-6">';
         html += '<h1>' + coffee.name + '</h1>';
-        html += '<p>' + coffee.roast + '</p>';
+        html += '<img class="light-roast" src="images/light.jpeg" alt="light coffee"></img>';
+        html += '<h5>' + coffee.roast + '</h5>';
+        html += '<p>' + coffee.price + '</p>';
         html += '</div>';
         return html;
+    } else if (coffee.roast === 'Medium'){
+        let html = '<div class="coffee col-6">';
+        html += '<h1>' + coffee.name + '</h1>';
+        html += '<img class="light-roast" src="images/medium.jpeg" alt="light coffee"></img>';
+        html += '<h5>' + coffee.roast + '</h5>';
+        html += '<p>' + coffee.price + '</p>';
+        html += '</div>';
+        return html;
+    } else if (coffee.roast === 'Dark'){
+        let html = '<div class="coffee col-6">';
+        html += '<h1>' + coffee.name + '</h1>';
+        html += '<img class="light-roast" src="images/dark.jpeg" alt="light coffee"></img>';
+        html += '<h5>' + coffee.roast + '</h5>';
+        html += '<p>' + coffee.price + '</p>';
+        html += '</div>';
+        return html;
+    }
 }
 
 //THIS FUNCTION ADDS COFFEE TO THE TABLE
@@ -52,30 +62,51 @@ let tbody = document.querySelector('#coffees');
 
 //THIS FUNCTION STORES ID OF R0AST-SELECTION AS ROAST SELECTION
 let roastSelection = document.querySelector('#roast-selection');
+
 // DISPLAYS ALL COFFEE IN HTML
 tbody.innerHTML = renderCoffees(coffees);
 
+
+let search = document.querySelector('#search-coffee')
+search.addEventListener('input', updateCoffees);
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     let selectedRoast = roastSelection.value;
     let filteredCoffees = [];
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === selectedRoast && coffee.name.toLowerCase().includes(search.value.toLowerCase())) {
             filteredCoffees.push(coffee);
-        } else if (coffee.roasted === selectedRoast) {
+        } else if (coffee.roasted === selectedRoast && coffee.name.toLowerCase().includes(search.value.toLowerCase())) {
             filteredCoffees.push(coffee);
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
-
-// let submitButton = document.querySelector('#submit');
-
-// submitButton.addEventListener('click', updateCoffees);
-
 //Add functionality to search through the coffees by name, and display only the coffees that match the provided search term (You will need to add an input field to the existing form for this)
-window.addEventListener("DOMContentLoaded", function(){
+addEventListener("DOMContentLoaded", function(){
     document.getElementById('roast-selection').addEventListener("change", updateCoffees)
 });
 
-//This adds functionality to update the displayed coffee as the user types into the search box, or as soon as they select an option from the select.
+// let userRoast = document.querySelector('#roast-selection-2');
+// let userCoffeeName = document.querySelector('#user-choice');
+// let userSubmitButton = document.querySelector('#user-submit');
+// userSubmitButton.addEventListener('click', userCoffeeSubmission);
+
+let userRoast = document.querySelector('#roast-selection-2');
+let addToDoButton = document.getElementById('user-submit');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('input-Field');
+
+addToDoButton.addEventListener('click', function(){
+    // let paragraph = document.createElement('p')
+    // paragraph.classList.add('paragraph-styling');
+    // paragraph.innerText = inputField.value;
+    // toDoContainer.appendChild(paragraph);
+    // inputField.value = "";
+        coffees.push({id: coffees.length + 1, name: inputField.value, roast: userRoast.value, roasted: "All", price: "$2.50"})
+        // updateCoffees(coffees);
+        tbody.innerHTML = renderCoffees(coffees);
+
+
+})
+
